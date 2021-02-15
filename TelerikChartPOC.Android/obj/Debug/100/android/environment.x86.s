8a547d954f1b4369adf5a5db86337e162e4406a4
Rama Bhandaru <microrama@gmail.com>
2021-02-15 11:57:00 -0500

    commit first cut

--- TelerikChartPOC.Android/obj/Debug/100/android/environment.x86.s
+++ TelerikChartPOC.Android/obj/Debug/100/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"657f5e9e-ba28-478c-bd02-d8aa4dba1785"
+	.asciz	"b0d97c74-14fc-4f44-a3c1-44a379cc5614"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
